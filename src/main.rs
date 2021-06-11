5da1a1b891cbd1c8540736b7a6c8578b9b676ac4
Brooks Patton <brooks@brookzerker.com>
2021-06-11 08:49:54 -0600

    Rocket works well so far

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{
+    form::{Form, Lenient, Strict},
+    http::{ContentType, Status},
+    response::content::Json,
+    Response,
+};
+use serde::{Deserialize, Serialize};
+
+#[macro_use]
+extern crate rocket;
+
+#[derive(FromForm, Debug)]
+struct Data {
+    message: Strict<String>,
+    id: Option<u32>,
+}
+
+#[derive(Serialize, Responder)]
+struct JsonResponse {
+    data: String,
+}
+
+#[derive(Debug, Serialize)]
+struct Game {
+    id: u32,
+    name: String,
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello world"
+}
+
+#[post("/games", data = "<data>")]
+fn create_game(data: Form<Data>) -> Json<JsonResponse> {
+    dbg!(&data);
+    let game = Game {
+        id: data.id.unwrap_or(0),
+        name: data.message.clone(),
+    };
+    Json(JsonResponse {
+        data: serde_json::to_string(&game).unwrap(),
+    })
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, create_game])
 }
