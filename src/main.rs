2d14cd6153a22e2b1d631966844b0b111ab8c729
Brooks Patton <brooks@brookzerker.com>
2021-06-15 08:25:23 -0600

    we have sse working
    sending a string every second

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
+use rocket::async_stream::yielder::Sender;
+use rocket::response;
+use rocket::response::content::Html;
+use rocket::response::stream::EventStream;
+use rocket::tokio::time::{self, Duration};
+use rocket::Shutdown;
+use rocket::State;
 use rocket::{
     form::{Form, Lenient, Strict},
     http::{ContentType, Status},
-    response::content::Json,
+    response::{
+        content::Json,
+        stream::{Event, ReaderStream, TextStream},
+    },
+    tokio::net::TcpStream,
     Response,
 };
 use serde::{Deserialize, Serialize};
+use std::{io, net::SocketAddr};
 
 #[macro_use]
 extern crate rocket;
@@ -27,8 +39,8 @@ struct Game {
 }
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello world"
+fn index() -> Html<&'static str> {
+    Html("<html><body><h1>Hello</h1></body></html>")
 }
 
 #[post("/games", data = "<data>")]
@@ -43,7 +55,40 @@ fn create_game(data: Form<Data>) -> Json<JsonResponse> {
     })
 }
 
+#[get("/stream")]
+async fn stream() -> io::Result<ReaderStream![TcpStream]> {
+    let address = SocketAddr::from(([127, 0, 0, 1], 9999));
+    let stream = TcpStream::connect(address).await?;
+    Ok(ReaderStream::one(stream))
+}
+
+/// Produce an infinite series of `"hello"`s, one per second.
+#[get("/infinite-hellos")]
+async fn infinite_hello() -> TextStream![&'static str] {
+    TextStream! {
+        let mut interval = time::interval(Duration::from_secs(1));
+        loop {
+            yield "hello";
+            interval.tick().await;
+        }
+    }
+}
+
+#[get("/infinite_events")]
+async fn infinite_events() -> EventStream![] {
+    EventStream! {
+        let mut interval = time::interval(Duration::from_secs(1));
+        loop {
+            yield Event::data("hello");
+            interval.tick().await;
+        }
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, create_game])
+    rocket::build().mount(
+        "/",
+        routes![index, create_game, stream, infinite_hello, infinite_events],
+    )
 }
